351022cdb84dc3f74184da8e590e4b0114f80eaa
xiaoxudong [肖旭东] <xiaoxudong@xiaomi.com>
2017-12-23 16:21:37 +0800

    build-2017-12-23-16-21-37

--- git.sh
+++ git.sh
@@ -7,9 +7,6 @@ npm run build                           # webpack构建到dist目录
 echo "build end"                        # 构建结束
 pack_time=$(date +%Y-%m-%d-%H-%M-%S)    # 构建时间
 pack_name=build-$pack_time              # 压缩包名称
-rm -rf ./dist.tgz                       #删除之前的压缩包
-tar czf ./dist.tgz dist                 # 打一个压缩包
 
 git add -A && git commit -am "$pack_name"
-#git push origin master                  # 提交
-#rm -rf dist                             # 删除output
+git push origin master                  # 提交
