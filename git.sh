3a2d1405ec837fe176b661a4944ba33aa6312bb2
xiaoxudong [肖旭东] <xiaoxudong@xiaomi.com>
2017-12-23 16:22:51 +0800

    build-2017-12-23-16-22-51

--- git.sh
+++ git.sh
@@ -2,9 +2,6 @@
 # @author xiaoxudoo from leike 2017-02-17
 # 自动打包上传到代码仓库
 
-echo "build start"                      # 开始构建
-npm run build                           # webpack构建到dist目录
-echo "build end"                        # 构建结束
 pack_time=$(date +%Y-%m-%d-%H-%M-%S)    # 构建时间
 pack_name=build-$pack_time              # 压缩包名称
 
